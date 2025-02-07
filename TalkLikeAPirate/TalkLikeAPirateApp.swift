5f4cb1218346f009c5d12cb81d9e725d32b104ba
DoodMac <48062579+jbird84@users.noreply.github.com>
2025-02-06 19:17:46 -0600

    Initial Commit

---
+++ TalkLikeAPirate/TalkLikeAPirateApp.swift
@@ -0,0 +1,17 @@
+//
+//  TalkLikeAPirateApp.swift
+//  TalkLikeAPirate
+//
+//  Created by Kinney Kare on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct TalkLikeAPirateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TalkLikeAPirateTests/TalkLikeAPirateTests.swift
@@ -0,0 +1,17 @@
+//
+//  TalkLikeAPirateTests.swift
+//  TalkLikeAPirateTests
+//
+//  Created by Kinney Kare on 2/6/25.
+//
+
+import Testing
+@testable import TalkLikeAPirate
+
+struct TalkLikeAPirateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
